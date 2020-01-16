a55be2c4d4f3b6888bd7011d74d09bc75b3a7499
aeckes <drusbury@outlook.com>
2020-01-15 20:09:55 -0800

    adding subset data

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,12 +1,16 @@
-function createMap(fireLayer) {
-
+//function createMap(fireLayer) {
+  var map = L.map("map-id", {
+    center: [-35.8177528,137.0530524],
+    zoom: 10,
+    //layers: [lightmap, fireLayer]
+  });
 // tile layer
   var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.light",
+    id: "mapbox://styles/drusbury/ck5g5nug90ajz1jqmzcvyxix1",
     accessToken: API_KEY
-  });
+  }).addTo(map);
 
   // Create a baseMaps object to hold the lightmap layer
   var baseMaps = {
@@ -19,10 +23,10 @@ function createMap(fireLayer) {
   };
 
   // Create the map object with options
-  var map = L.map("map-id", {
-    center: [-35.8177528,137.0530524],
-    zoom: 10,
-    layers: [lightmap, fireLayer]
-  });
 
-}
+
+//}
+
+
+
+d3.read_csv()
