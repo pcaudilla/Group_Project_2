26f673e816a45913a196eaa74b91e7495bb01b5c
cheriemosher <mosher.cherie@hushmail.com>
2020-01-22 21:28:21 -0800

    updated javascript for heatmap

--- mosher_playing/maps.js
+++ mosher_playing/maps.js
@@ -1,33 +1,32 @@
-var heatMapLayer;
+var heatArray = []
+var malleeArray = []
 
 d3.csv(firePath, function (data) {
 
-    var heatArray = []
-
     for (var i = 0; i < data.length; i++) {
-      console.log(data.length);
+      
       var lat = +data[i].latitude;
       var lng = +data[i].longitude;
 
       var latlng = L.latLng([lat, lng]);
       heatArray.push(latlng);
+    }
       
-    };
-
-    heatMapLayer = L.heatLayer(heatArray, {
-      radius: 8,
-      blur: 10,
-      minOpacity: 2,
-      maxZoom: 20,
-      gradient: {
-        0.8: 'orange',
-        1.0: 'red'
-      }
-
-
-    });
-
 });
+
+d3.csv(malleeObs, function (data) {
+
+    for (var i = 0; i < data.length; i++) {
+      console.log(data.length);
+      var lat = +data[i].Latitude;
+      var lng = +data[i].Longitude;
+      
+      var latlng = L.latLng([lat, lng]);
+      malleeArray.push(latlng);
+    }
+      
+});
+
 
 var layers = {
     layer1: {
@@ -47,22 +46,48 @@ var layers = {
         attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
         id: "mapbox://styles/drusbury/ck5g5nug90ajz1jqmzcvyxix1",
         legend: '<i style="background: black; opacity: 0.5"></i><p><b>legend 1</b></p>'
-    },
+    }
+};
 
+var overlayLayers = {
+    heatLayer: {
+        layer: L.heatLayer(heatArray, {
+            radius: 8,
+            blur: 10,
+            minOpacity: 2,
+            maxZoom: 20,
+            gradient: {
+                0.8: 'orange',
+                1.0: 'red'
+            }
+        })
+    },
+    malleeLayer: {
+        layer: L.heatLayer(malleeArray, {
+            radius: 3,
+            blur: 3,
+            minOpacity: 100,
+            maxZoom: 20,
+            gradient: {
+                0.0: 'black',
+                1.0: 'black'
+            }
+        })
+    },
 };
 
 
-
+console.log(malleeArray)
 var scenes = {
-    scene1: {lat: -25.274398, lng: 133.775136, zoom: 4.5, layers: [layers.layer3], name: "Australia"},
+    scene1: {lat: -25.274398, lng: 133.775136, zoom: 4.5, layers: [layers.layer2], name: "Australia"},
     scene2: {lat: -35.8177, lng: 137.05305, zoom: 8, layers: [layers.layer2], name: "Southern Australia"},
     scene3: {lat: -35.8177, lng: 137, zoom: 9.55, layers: [layers.layer1, layers.layer2], name: "Kangaroo Island"},
-    scene4: {lat: -35.8177, lng: 137, zoom: 9.55, layers: [layers.layer2, heatMapLayer], name: "scene 4"},
-    scene5: {lat: -35.8177, lng: 137, zoom: 9.55, layers: [layers.layer2], name: "scene 5"},
-    scene6: {lat: -35.8177, lng: 137, zoom: 9.55, layers: [layers.layer2], name: "scene 6"},
-    scene7: {lat: -35.8177, lng: 137, zoom: 9.55, layers: [layers.layer2], name: "scene 7"},
-    scene8: {lat: -35.8177, lng: 137, zoom: 9.55, layers: [layers.layer2], name: "scene 8"},
-    scene9: {lat: -35.8177, lng: 137, zoom: 9.55, layers: [layers.layer2], name: "scene 9"}
+    scene4: {lat: -35.8177, lng: 137, zoom: 9.55, layers: [layers.layer2, overlayLayers.heatLayer], name: "Wildfires"},
+    scene5: {lat: -35.8177, lng: 137, zoom: 9.55, layers: [layers.layer2, overlayLayers.heatLayer, overlayLayers.malleeLayer], name: "Mallee"},
+    scene6: {lat: -35.8177, lng: 137, zoom: 9.55, layers: [layers.layer2, overlayLayers.heatLayer], name: "scene 6"},
+    scene7: {lat: -35.8177, lng: 137, zoom: 9.55, layers: [layers.layer2, overlayLayers.heatLayer], name: "scene 7"},
+    scene8: {lat: -35.8177, lng: 137, zoom: 9.55, layers: [layers.layer2, overlayLayers.heatLayer], name: "scene 7"},
+    scene9: {lat: -35.8177, lng: 137, zoom: 9.55, layers: [layers.layer2, overlayLayers.heatLayer], name: "scene 7"}
 };
 
 $('#storymap').storymap({
