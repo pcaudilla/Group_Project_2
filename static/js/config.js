8d352588314a5df12da54e20fa95c4e9dbd9a327
aeckes <drusbury@outlook.com>
2020-01-25 09:11:48 -0800

    quick file rename

--- static/js/config.js
+++ static/js/config.js
@@ -2,7 +2,7 @@ const API_KEY = "pk.eyJ1IjoiZHJ1c2J1cnkiLCJhIjoiY2s0endjNG0zMGZ0bDNucWg1MXhjb2ti
 const MAPBOX_URL = "https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}";
 const ATTRIBUTION = "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>";
 var firePath = "static/data/fire_data/COMBINED_KI.csv"
-var firePathAUS = "static/data/fire_data/MODIS_AUS - Copy.csv"
+var firePathAUS = "static/data/fire_data/MODIS_AUS.csv"
 var malleeObs = "static/data/species_locations/eucalyptus.csv"
 var echidnaObs = "static/data/species_locations/echidna.csv"
 var termiteObs = "static/data/species_locations/termite.csv"
